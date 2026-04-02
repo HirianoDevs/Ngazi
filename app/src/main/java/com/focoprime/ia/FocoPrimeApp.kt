a901f91e782e43de50b608e7aaf629c4c489883c
HirianoDevs <hirianodevs@gmail.com>
2026-04-02 22:30:37 +0200

    Create FocoPrimeApp.kt

---
+++ app/src/main/java/com/focoprime/ia/FocoPrimeApp.kt
@@ -0,0 +1,10 @@
+package com.focoprime.ia
+
+import android.app.Application
+
+class FocoPrimeApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Aqui você pode iniciar tua IA futuramente
+    }
+}
